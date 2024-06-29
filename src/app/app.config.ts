94fe68f1c54a1702f7df2f13badb203887fac47e
aronfazekas <fazekasaron95@gmail.com>
2024-06-29 16:41:29 +0200

    improve book-list

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { provideRouter } from '@angular/router';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), ReactiveFormsModule]
+  providers: [provideRouter(routes), ReactiveFormsModule, provideAnimationsAsync()]
 };
--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
 import { RouterLink } from '@angular/router';
+import { Book } from '../service/book';
+import { Genre } from '../service/genre';
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgFor, MatTableModule, MatButtonModule, MatIconModule],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.scss'
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit {
 
+  displayedColumns: string[] = ['author', 'title', 'publisher', 'year', 'description', 'genre', 'edit'];
+  dataSource = BOOK_DATA;
+
+  ngOnInit(): void {
+    // Fetch or set your data here
+  }
+
+  editBook(book: Book): void {
+    console.log('Edit book:', book);
+    // Implement your edit logic here
+  }
+
+  addBook() {
+    console.log('Add new book');
+  }
 }
+
+const BOOK_DATA: Book[] = [
+  {
+    id: 1,
+    author: 'Author 1',
+    title: 'Title 1',
+    publisher: 'Publisher 1',
+    year: 2020,
+    description: 'Description 1',
+    genre: Genre.ChildrenStory
+  },
+];
